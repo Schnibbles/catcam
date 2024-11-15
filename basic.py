55e0db4a7bd8399c7f541a33b67cfd4e2b9ab215
Schnibbles <kurt@scheibl.uk>
2024-11-15 18:11:06 +0000

    Change to streaming approach

--- basic.py
+++ basic.py
@@ -3,6 +3,17 @@ import sys
 import time
 from typing import List
 import socket
+import argparse
+import sys
+from functools import lru_cache
+
+import cv2
+import numpy as np
+
+from picamera2 import MappedArray, Picamera2
+from picamera2.devices import IMX500
+from picamera2.devices.imx500 import (NetworkIntrinsics,
+                                      postprocess_nanodet_detection)
 
 import cv2
 import numpy as np
