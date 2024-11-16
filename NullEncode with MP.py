f78a0223c55c7e82f65c132743b9b1b6ae70b730
Schnibbles <kurt@scheibl.uk>
2024-11-16 14:39:54 +0000

    Change to streaming approach

--- NullEncode with MP.py
+++ NullEncode with MP.py
@@ -72,7 +72,8 @@ def draw_detections(jobs):
     labels = get_labels()
     # Wait for result from child processes in the order submitted.
     last_detections = []
-    while (job := jobs.get()) is not None:
+#    while (job := jobs.get()) is not None:
+    while True:
         request, async_result = job
         detections = async_result.get()
         if detections is None:
